7d4b741b9c51f47a52a3680eddb2afccbe3ffaad
harkat <harkatamir@gmail.com>
2016-06-09 16:16:41 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,16 +4,6 @@ import java.util.Scanner;
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
  *  can walk around some scenery. That's all. It should really be extended 
  *  to make it more interesting!
- * 
- *  To play this game, create an instance of this class and call the "play"
- *  method.
- * 
- *  This main class creates and initialises all the others: it creates all
- *  rooms, creates the parser and starts the game.  It also evaluates and
- *  executes the commands that the parser returns.
- * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2011.07.31
  */
 
 public class Game 
